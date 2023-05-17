20768041d5cb0963b55bdaa1061cf673c4ba807e
Ryan Jolley <ryan.jolley0@gmail.com>
2023-05-17 14:52:30 -0600

    reverted gitgub files

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const { sequelize } = require("./util/database");
 const { User, Spots } = require("./models/tables");
+
 const { PORT } = process.env;
-const {CONNECTION_STRING} = process.env;
 
 const { isAuthenticated } = require("./middleware/isAuth");
 const { register, login } = require("./controllers/authCtrl");
@@ -17,12 +17,12 @@ app.use(cors());
 User.hasMany(Spots);
 Spots.belongsTo(User);
 
-app.post(`${CONNECTION_STRING}/register`, register);
-app.post(`${CONNECTION_STRING}/login`, login);
+app.post("/register", register);
+app.post("/login", login);
 
-app.get(`${CONNECTION_STRING}/spots/:userId`, getAllSpots);
-app.post(`${CONNECTION_STRING}/spots`, isAuthenticated, addSpot);
-app.delete(`${CONNECTION_STRING}/spots/:id`, deleteSpot);
+app.get("/spots/:userId", getAllSpots);
+app.post("/spots", isAuthenticated, addSpot);
+app.delete("/spots/:id", deleteSpot);
 
 sequelize
   .sync()
--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Routes, Route, Navigate} from "react-router-dom"
 import {useContext} from 'react'
 import AuthContext from "./store/authContext";
+
 import "./App.css"
 
 import Header from "./components/Header";
@@ -8,7 +9,24 @@ import Home from './components/Home';
 import Auth from './components/Auth';
 import AddSpots from './components/AddSpots';
 
+const path = require('path')
+const _dirname = path.dirname("")
+const buildPath = path.join(_dirname  , "../client/build");
 
+app.use(express.static(buildPath))
+
+app.get("/*", function(req, res){
+
+    res.sendFile(
+        path.join(__dirname, "../client/build/index.html"),
+        function (err) {
+          if (err) {
+            res.status(500).send(err);
+          }
+        }
+      );
+
+})
 
 
 function App() {
